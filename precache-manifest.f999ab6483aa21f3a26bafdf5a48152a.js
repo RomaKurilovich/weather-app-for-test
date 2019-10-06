01987802f49008852b5fc121abbea5f11e31768a
Roman <kamazaff22@gmail.com>
2019-10-06 21:10:14 +0300

    add gh-pages

---
+++ precache-manifest.f999ab6483aa21f3a26bafdf5a48152a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccd6a5252e79804ac14a37374ede534",
+    "url": "/weather-app-for-test/index.html"
+  },
+  {
+    "revision": "a3b548bdade24e4312f5",
+    "url": "/weather-app-for-test/static/css/main.519a9f56.chunk.css"
+  },
+  {
+    "revision": "22be17c6368d0a9f682c",
+    "url": "/weather-app-for-test/static/js/2.99a51069.chunk.js"
+  },
+  {
+    "revision": "a3b548bdade24e4312f5",
+    "url": "/weather-app-for-test/static/js/main.9c7f9221.chunk.js"
+  },
+  {
+    "revision": "4bd8798c993ce4535b79",
+    "url": "/weather-app-for-test/static/js/runtime~main.ebb78e15.js"
+  },
+  {
+    "revision": "bbbf9ccf09cb1333a68aa263b87c573e",
+    "url": "/weather-app-for-test/static/media/pl.bbbf9ccf.jpg"
+  }
+]);
